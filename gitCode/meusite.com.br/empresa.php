b441a3e85a3609325ac35ec8efa922500b5a2934
Ronaldo Marins <ronaldo.microsiga@gmail.com>
2014-10-19 01:38:18 -0300

    commit total do projeto fase 1 - foundation

---
+++ gitCode/meusite.com.br/empresa.php
@@ -0,0 +1,9 @@
+<?php
+require_once('verificapagina.php');
+
+require_once("menu.php");
+?>
+
+<h1>Empresa!!</h1>
+<p>Carregou a Empresa</p>
+<?php require_once("rodape.php"); ?>
---
+++ gitCode/meusite.com.br/home.php
@@ -0,0 +1,9 @@
+<?php
+require_once('verificapagina.php');
+
+require_once("menu.php");
+?>
+
+<h1>home!!</h1>
+<p>Carregou o home</p>
+<?php require_once("rodape.php"); ?>
