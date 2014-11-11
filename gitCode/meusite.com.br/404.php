fb187ce0afe4597ec0fc9a4e72cdc582d678416b
Ronaldo Marins <ronaldo.microsiga@gmail.com>
2014-11-11 02:15:35 -0300

    projeto pdo

--- gitCode/meusite.com.br/404.php
+++ gitCode/meusite.com.br/404.php
@@ -1,10 +1,3 @@
-<h1>Página não encontrada</h1>
-
-<?php
-
-header("Status: 404 Not Found");
-//header("HTTP/1.0 404 Not Found");
-//http_response_code(404);
 
 <!DOCTYPE HTML>
 <html lang="pt-br">
@@ -12,8 +5,17 @@ header("Status: 404 Not Found");
     <meta charset="UTF-8">
     <title>Site Simples do Curso Code Education</title>
 
+<h1>Página não encontrada</h1>
+
 </head>
+
 <body>
+<?php
+
+header("Status: 404 Not Found");
+//header("HTTP/1.0 404 Not Found");
+//http_response_code(404);
+?>
 
 </body>
 
